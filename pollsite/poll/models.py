1b4187262740e44403bb31574d7e8586b31abe7a
RduMarais <rmichon@telecom-paristech.fr>
2021-12-24 00:01:03 +0100

    created Questions as a abstract model and extended with 3 models

--- pollsite/poll/models.py
+++ pollsite/poll/models.py
@@ -10,6 +10,9 @@ class Question(models.Model):
     featured = models.BooleanField('Feature in Featured Polls Page')
     is_public = models.BooleanField('Is public in polls page',default=False)
 
+    class Meta:
+        abstract=True
+
     def __str__(self):
         return self.title
 
@@ -30,9 +33,31 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+
+##### POLL MODEL #####
+class Poll(Question):
+    question_type = "Poll"
+
+class PollChoice(Choice):
+    question = models.ForeignKey(Poll,on_delete=models.CASCADE)
+
+##### QUIZZ MODEL #####
+class Quizz(Question):
+    question_type = "Quizz"
+
+class QuizzChoice(Choice):
+    question = models.ForeignKey(Quizz,on_delete=models.CASCADE)
+
+##### WORDCLOUD MODEL #####
+class WordCloud(Question):
+    question_type = "WordCloud"
+
+class WordCloudChoice(Choice):
+    question = models.ForeignKey(WordCloud,on_delete=models.CASCADE)
