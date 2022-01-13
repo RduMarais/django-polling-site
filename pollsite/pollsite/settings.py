91e6e5feda5c238d048f3f8a92b575cf242f2a79
RduMarais <rmichon@telecom-paristech.fr>
2022-01-13 17:47:18 +0100

    made the app runnable with uvicorn

--- pollsite/pollsite/settings.py
+++ pollsite/pollsite/settings.py
@@ -131,10 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR + "/static",
-    # '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATICFILES_DIRS = [
+#     BASE_DIR + "/static",
+#     # '/var/www/static/',
+# ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
 
