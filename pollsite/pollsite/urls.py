c5b51f40cf1b5ec9ed29f45ec7f302526e0aad9f
RduMarais <rmichon@telecom-paristech.fr>
2022-01-13 18:00:42 +0100

    same, but for real

--- pollsite/pollsite/urls.py
+++ pollsite/pollsite/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('poll/', include('poll.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #TODO for dev only
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
