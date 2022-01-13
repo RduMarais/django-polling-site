e95205bfb3cd475b140d5bdee2742fb2e4f0d9ad
RduMarais <rmichon@telecom-paristech.fr>
2022-01-13 18:14:36 +0100

    defined websocket handling

--- pollsite/pollsite/asgi.py
+++ pollsite/pollsite/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from pollsite.websocket import websocket_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollsite.settings')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        # Let Django handle HTTP requests
+        await django_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        # We handle Websocket connections here
+        await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
---
+++ pollsite/pollsite/websocket.py
@@ -0,0 +1,24 @@
+"""
+Websocket Handler for Pollsite
+
+https://jaydenwindle.com/writing/django-websockets-zero-dependencies/
+"""
+
+async def websocket_application(scope, receive, send):
+	while True:
+		event = await receive()
+
+		if(event['type'] == 'websocket.connect'):
+			await send({
+				'type': 'websocket.accept'
+			})
+		
+		if(event['type'] == 'websocket.disconnect'):
+			break
+		
+		if(event['type'] == 'websocket.receive'):
+			if(event['text'] == 'ping'):
+				await send({
+					'type': 'websocket.send',
+					'text': 'pong!'
+				})
